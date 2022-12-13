0763f5a15922d6412133aedd79fe269731792209
Sai Nath <92171156+sai-na@users.noreply.github.com>
2022-12-13 14:50:09 +0530

    String Tokenize

---
+++ StringTokenizerDemo.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class StringTokenizerDemo
+{
+public static void main(String[] args)
+{
+int sum=0;
+Scanner scan=new Scanner(System.in);
+System.out.println("Enter digits with a Space Gap : ");
+String str=scan.nextLine();
+StringTokenizer st=new StringTokenizer(str," ");
+while(st.hasMoreTokens())
+{
+String temp=st.nextToken();
+int n=Integer.parseInt(temp);
+System.out.println(n);
+sum=sum+n;
+}
+System.out.println(sum);
+}
+}
+
+
+
